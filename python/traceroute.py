201d4c487b4968135ac105f3f3dfed8bbe6f1684
gere <gerevini@gmail.com>
2017-05-15 22:39:45 +0200

    main traceroute now in function, started work on packet analysis

--- python/traceroute.py
+++ python/traceroute.py
@@ -6,123 +6,69 @@ import os
 from struct import *
 
 def listen():
-	s1 = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
-	s1.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
-	while 1:
-		data, addr = s1.recvfrom(1508)
-		print ("Packet from %r: %r" % (addr,data))
 
-HOST = socket.gethostbyname('google.it')
-PORT = 80	
-ADDR = (HOST, PORT)
-ttl = 1
-timeout = 0
-try:
-	dgram_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-except socket.error as msg: 
-	print('send socket no created. Errror code:' + str(msg))
-	sys.exit()
+	HOST = socket.gethostbyname('google.it')
+	PORT = 80	
+	ADDR = (HOST, PORT)
+	ttl = 1
+	timeout = 0
+	try:
+		dgram_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	except socket.error as msg: 
+		print('send socket no created. Errror code:' + str(msg))
+		sys.exit()
 
-try:
-	listen_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,socket.IPPROTO_ICMP)
-except socket.error as msg: 
-	print('listen socket no created. Errror code:' + str(msg))
-	sys.exit()
+	try:
+		listen_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,socket.IPPROTO_ICMP)
+	except socket.error as msg: 
+		print('listen socket no created. Errror code:' + str(msg))
+		sys.exit()
 
-listen_socket.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
-listen_socket.settimeout(3)
+	listen_socket.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
+	listen_socket.settimeout(3)
 
-while ttl < 100:	
+	while ttl < 100:	
 
-	if (timeout >= 3):
-		ttl += 1
-		timeout = 0	
-		"""tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		tcp_socket.connect(ADDR)
-		tcp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl	- 1)
-		tcp_socket.sendall(b"GET /\n\n")
+		if (timeout >= 3):
+			ttl += 1
+			timeout = 0		
+		
+		dgram_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+		
+		p = pack('!bbb', 1, 2, 3)
+		b = dgram_socket.sendto(p, ADDR)
 		try:
 			data, addr = listen_socket.recvfrom(1508)
 		except socket.timeout:
-			print("timeout at tcp hop", ttl, '\n')
-			timeout+=1
-			tcp_socket.close()
+			print("timeout at hop", ttl, '\n')
+			timeout += 1		
 			continue
-
-		ttl += 1
-		timeout = 0
-		tcp_socket.close()
-		continue"""
-	
-	dgram_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
-	
-	p = pack('hhl', 1, 2, 3)
-	b = dgram_socket.sendto(p, ADDR)
-	try:
-		data, addr = listen_socket.recvfrom(1508)
-	except socket.timeout:
-		print("timeout at hop", ttl, '\n')
-		timeout += 1		
-		continue
-	if (addr == HOST):
-		print("Arrived!\n")
-		
+		if (addr == HOST):
+			print("Arrived!\n")
+			
+			break
+		print("hop:", ttl, addr, "size:", sys.getsizeof(data), data)
 		break
-	print("hop:", ttl, addr)
-	ttl += 1
+		ttl += 1
 	
+"""
+example data of 105 byte size
+b'E\xc04\x00\xea\x8e\x00\x00@\x01{d\n\x00\x00\x01\n\x00\x00\x02\x0b\x00\xa5i\x00\x00\x00\x00E\x00,\x00\x81O\x00\x00\x01\x11\x882\n\x00\x00\x02\xd8:\xce\x03\xe5\xee\x00P\x00\x18c?\x01\x00\x02\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00'
+"""
+def unpack_packet(buffer):
+	#ip_header_format = "!bbhII4s4s"
+	ip_header_format = "!IIIIIbbhlh3s"
+	return unpack_from(ip_header_format, buffer)
 
+test_packet = b'E\xc04\x00\xea\x8e\x00\x00@\x01{d\n\x00\x00\x01\n\x00\x00\x02\x0b\x00\xa5i\x00\x00\x00\x00E\x00,\x00\x81O\x00\x00\x01\x11\x882\n\x00\x00\x02\xd8:\xce\x03\xe5\xee\x00P\x00\x18c?\x01\x00\x02\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00'
+test_packet2 = b"E\xc0'\x00\xac\xfa\x00\x00@\x01\xb9\x05\n\x00\x00\x01\n\x00\x00\x02\x0b\x00\xa5<\x00\x00\x00\x00E\x00\x1f\x00'P\x00\x00\x01\x11\xe2^\n\x00\x00\x02\xd8:\xcd\xe3\xd7x\x00P\x00\x0bs\xed\x01\x02\x03"
+#version_ihl, dscp_ecn, total_length, _, _, source_ip, dest_ip = unpack_packet(test_packet)
+_,_,_,_,_, typef, code, _,_,_, data = unpack_packet(test_packet2)
 
+print(typef)
+print(code)
+for x in data:
+	print(x.hex())
+#listen()
+#print(total_length)
 
-"""try:
-	s.connect((HOST, PORT))
-except socket.timeout as e:
-	print('timeout1', e)
-except socket.error as e:
-	print ('se1:', e)
-else:
-	s.sendall(b'GET / \n\n')
-	try:
-		data = s.recv(1024)
-	except socket.timeout as e:
-		print('timeout2', e)
-	except socket.error as e:
-		print ('se2:', e)
-	else:
-		print('Received', repr(data))
-	
-
-HOST = socket.gethostbyname('www.google.it')
-PORT = 33434
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-except socket.error as msg: 
-	print('socket no created. Errror code:' + str(msg))
-	sys.exit()
-
-s.bind((HOST, 0))
-s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-#s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-p = pack('hhl', 1, 2, 3)
-s.sendto(p, (HOST, PORT))
-data = s.recvfrom(1024)
-print('received: ', repr(data))
-
-# the public network interface
-HOST = socket.gethostbyname(socket.gethostname())
-
-# create a raw socket and bind it to the public interface
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-s.bind((HOST, 0))
-
-# Include IP headers
-s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-
-# receive all packages
-s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-# receive a package
-print(s.recvfrom(65565))
-
-# disabled promiscuous mode
-s.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)"""
